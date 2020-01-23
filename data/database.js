8d1cd0cc34394405780e8c1904371723e1556342
Dahan Schuster <dahan@moobitech.com.br>
2020-01-23 15:59:37 -0300

    Improves the database file
    Configures the database file to read and write a json file
    That works like a non-relational database,
    which has JSON files representing the tables

--- data/database.js
+++ data/database.js
@@ -1,21 +1,53 @@
+const fs = require('fs') // imports the file system module
+const path = __dirname + '/products.json' // the path to the products json
+
+let products = require('./products.json')
+
+/**
+ * gives an ID and increment it, so the next ID will be ID + 1
+ */
 const sequence = {
-    _id: 1,
+    _id: products.lastId,
 
     get id() {
-        return this._id++ 
+        return ++this._id 
     }
 }
 
-const products = {}
-
+/**
+ * Save a product in the products object
+ * and call the function that writes the
+ * object in the json file
+ * 
+ * @param {object} product 
+ */
 const saveProduct = product => {
-    if (!product.id) product.id = sequence.id
+    if (!product.id) {
+        product.id = sequence.id
+        products.lastId = product.id
+    }
     products[product.id] = product
+    _writeProducts()
     return product
 } 
 
+/**
+ * Write all the products object in the json file
+ */
+function _writeProducts() {
+    fs.writeFile(path, JSON.stringify(products), error => console.log('Error:', error))
+}
+
+/**
+ * Returns an product from the products object
+ * 
+ * @param {number} id 
+ */
 const getProductById = id => products[id] || {}
 
+/**
+ * Returns all the values from the products object
+ */
 const getProducts = () => Object.values(products)
 
 module.exports = { saveProduct, getProductById, getProducts }
